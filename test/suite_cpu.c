c55cb75c48476a3043d164e7ee2a502fe237cf81
Peter Wilhelmsson <2hdddg@gmail.com>
2018-11-24 10:34:21 +0100

    ROL tests

--- test/suite_cpu.c
+++ test/suite_cpu.c
@@ -1206,3 +1206,69 @@ int test_arithmetic_shift_left()
 
     return run_tests(tests, sizeof(tests) / sizeof(tests[0]));
 }
+
+/* ROL */
+int test_roll_left()
+{
+    struct test tests[] = {
+        {
+            .name = "Accumulator",
+            .instructions = { 0x2a /* ROL A */ },
+            .num_steps = 1,
+            .check_flags = true,
+            .check_reg_a = true,
+            .state = {
+                .reg_a = 0x83,
+                .flags = FLAG_NEGATIVE|FLAG_CARRY,
+            },
+            /* Checks that MSB goes into carry and that carry
+             * is padded to the right. */
+            .init_reg_a = 0xc1,
+            .init_flags = FLAG_CARRY,
+        },
+        {
+            .name = "Zero page",
+            .instructions = { 0x26, 0x01, },
+            .num_steps = 1,
+            .check_ram_at = 0x0001,
+            .check_ram_val = 0x41,
+            .init_flags = FLAG_CARRY,
+        },
+        {
+            .name = "Zero page, X",
+            .instructions = { 0x36, 0x00, },
+            .num_steps = 1,
+            .check_ram_at = 0x0001,
+            .check_ram_val = 0x40,
+            .init_reg_x = 0x01,
+            .init_flags = 0x00,
+        },
+        {
+            .name = "Absolute",
+            .instructions = { 0x2e, 0x03, 0x40, },
+            .num_steps = 1,
+            .check_flags = true,
+            .check_ram_at = 0x4003,
+            .check_ram_val = 0x06,
+            .state = {
+                .flags = 0x00,
+            },
+            .init_flags = 0x00,
+        },
+        {
+            .name = "Absolute, X",
+            .instructions = { 0x3e, 0x00, 0x40 },
+            .num_steps = 1,
+            .check_flags = true,
+            .check_ram_at = 0x4001,
+            .check_ram_val = 0x03,
+            .state = {
+                .flags = 0,
+            },
+            .init_reg_x = 0x01,
+            .init_flags = FLAG_CARRY,
+        },
+    };
+
+    return run_tests(tests, sizeof(tests) / sizeof(tests[0]));
+}
