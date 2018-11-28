c9d07d36117d152dc62fec05084052a8205ef1c5
Peter Wilhelmsson <2hdddg@gmail.com>
2018-11-28 20:38:23 +0100

    Comments, plan for vic

--- mem.h
+++ mem.h
@@ -5,10 +5,19 @@ struct mem_h;
 int mem_create(struct mem_h **mem);
 int mem_reset(struct mem_h *mem);
 
-/* Read memory from CPU perspective */
-uint8_t mem_get_cpu(struct mem_h *mem, uint16_t addr);
-/* Set memory from CPU */
-void mem_set_cpu(struct mem_h *mem, uint16_t addr, uint8_t val);
+/* CPU memory API */
+uint8_t mem_get_cpu(struct mem_h *mem,
+                    uint16_t addr);
+void mem_set_cpu(struct mem_h *mem,
+                 uint16_t addr,
+                 uint8_t val);
+
+/* VIC memory API */
+uint8_t mem_get_vic(struct mem_h *mem,
+                    uint16_t addr);
+void mem_set_vic(struct mem_h *mem,
+                 uint16_t addr,
+                 uint8_t val);
 
 void mem_destroy(struct mem_h *mem);
 
