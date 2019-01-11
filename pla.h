f4f1b1a7c5240570ff1fa5a6cf413e3128e27472
Peter Wilhelmsson <2hdddg@gmail.com>
2019-01-11 19:26:53 +0100

    More CPU port tests

--- pla.h
+++ pla.h
@@ -1,10 +1,9 @@
 /* Implementation of PLA, programmable logic array.
- * Controls which chip that is connected to the
- * data bus.
+ *
+ * Controls which chip that is connected to the data bus.
+ *
  * This implementation modifies mem pages with hooks.
  */
-
-
 
 #pragma once
 #include <stdint.h>
@@ -14,11 +13,13 @@ void pla_init(uint8_t *rom_kernal,
               uint8_t *rom_basic,
               uint8_t *rom_chargen);
 
+/* Pins from CPU address 0/1 that affects memory layout. */
 void pla_pins_from_cpu(bool loram_high,
                        bool hiram_high,
                        bool charen_high);
 
-
+/* For debugging */
 bool pla_is_basic_mapped();
 
+/* For debugging */
 void pla_stat();
--- test/suite_cpu_port.c
+++ test/suite_cpu_port.c
@@ -5,18 +5,19 @@
 #include "mem.h"
 
 uint8_t _val;
-bool _loram_high;
-bool _hiram_high;
-bool _charen_high;
+
+bool _loram;
+bool _hiram;
+bool _charen;
 
 /* Mocked PLA implementation */
-void pla_pins_from_cpu(bool loram_high,
-                       bool hiram_high,
-                       bool charen_high)
+void pla_pins_from_cpu(bool loram,
+                       bool hiram,
+                       bool charen)
 {
-    _loram_high = loram_high;
-    _hiram_high = hiram_high;
-    _charen_high = charen_high;
+    _loram  = loram;
+    _hiram  = hiram;
+    _charen = charen;
 }
 
 int each_before()
@@ -24,9 +25,10 @@ int each_before()
     mem_init();
     mem_reset();
     _val = 0xff;
-    _loram_high = false;
-    _hiram_high = false;
-    _charen_high = false;
+    _loram= false;
+    _hiram= false;
+    _charen= false;
+    cpu_port_init();
     return 0;
 }
 
@@ -40,43 +42,40 @@ static int assert_val(uint8_t actual, uint8_t expected)
     return 1;
 }
 
-
-/* Will reach 0x2f after some cycles or by kernal ? */
-#if 0
-int test_direction_values()
-{
-    /* Value before initializing port should be zero */
-    _val = mem_get_for_cpu(0x00);
-    if (!assert_val(_val, 0x00)) {
-        printf("Should be zero before port init\n");
-        return 0;
-    }
-
-    cpu_port_init();
-    _val = mem_get_for_cpu(0x00);
-    return assert_val(_val, 0x2f);
-
-    return 1;
-}
-#endif
-
 int test_init_peripheral_values()
 {
-    /* Value before initializing port should be zero */
-    _val = mem_get_for_cpu(0x01);
-    if (!assert_val(_val, 0x00)) {
-        printf("Should be zero before port init\n");
-        return 0;
-    }
-
-    cpu_port_init();
     _val = mem_get_for_cpu(0x01);
     return assert_val(_val, 0x37);
 }
 
+/* Verifies that initial values will set all pins to PLA
+ * to high. */
 int test_init_sets_mem_pins_on_pla()
 {
-    cpu_port_init();
+    /* Call to cpu_port_init is done in setup */
+    return _loram && _hiram && _charen;
+}
 
-    return _loram_high && _hiram_high && _charen_high;
+/* Verifies that when direction is set to out and values
+ * are written from CPU those values are sent to PLA. */
+int test_direction_out_sets_custom_value_on_pla()
+{
+    uint8_t direction = CPU_PORT_LORAM | CPU_PORT_HIRAM |
+                        CPU_PORT_CHAREN;
+
+    /* At this point direction is still IN so this shouldn't
+     * affect PLA. */
+    mem_set_for_cpu(0x0001, 0x00);
+    if (!_loram || !_hiram || !_charen) {
+        printf("Expected pins to stay high\n");
+        return 0;
+    }
+
+    mem_set_for_cpu(0x0000, direction);
+    if (!(_loram && _hiram && _charen)) {
+        printf("Expected pins to go low\n");
+        return 0;
+    }
+
+    return 1;
 }
